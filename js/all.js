2c5e02bba4d83d37c87aca726c906750d61c8563
xinyu <lnxinyu19@gmail.com>
2021-03-10 12:17:49 +0800

    點擊分頁會滾動視窗

--- js/all.js
+++ js/all.js
@@ -1,7 +1,8 @@
 // 全域使用變數
 let jsonData = [];
 let noRepeatPlace = [];
-let pageData = []; //存放點選需顯示的分頁資料
+let pageData = [];
+//存放點選需顯示的分頁資料
 
 
 // 綁定DOM
@@ -97,7 +98,9 @@ function creatAreaOption() {
 }
 
 function compliedData() {
-  // let pageData = []
+  cardInfo.innerHTML = '' // 每次資料變動需清空顯示內容
+  areaInfo.innerHTML = '' 
+  pageData = []
   let selected = areaOption.value;
   for (let i = 0; i < jsonData.length; i++) {
     if (jsonData[i].animal_sex === "M") {
@@ -118,43 +121,24 @@ function compliedData() {
 
 function pagination(pageData, nowPage) {
   const dataTotal = pageData.length;
-  // 取得全部資料長度
-  // 設定要顯示在畫面上的資料數量
-  // 預設每一頁只顯示 20 筆資料。
-  const perpage = 20;
-
-  // page 按鈕總數量公式 總資料數量 / 每一頁要顯示的資料
-  // 這邊要注意，因為有可能會出現餘數，所以要無條件進位。
+  const perpage = 40;
   const pageTotal = Math.ceil(dataTotal / perpage);
 
-  // 當前頁數，對應現在當前頁數
   let currentPage = nowPage;
-
-  // 因為要避免當前頁數筆總頁數還要多，假設今天總頁數是 3 筆，就不可能是 4 或 5
-  // 所以要在寫入一個判斷避免這種狀況。
-  // 當"當前頁數"比"總頁數"大的時候，"當前頁數"就等於"總頁數"
-  // 注意這一行在最前面並不是透過 nowPage 傳入賦予與 currentPage，所以才會寫這一個判斷式，但主要是預防一些無法預期的狀況，例如：nowPage 突然發神經？！
   if (currentPage > pageTotal) {
     currentPage = pageTotal;
   }
 
   const minData = currentPage * perpage - perpage + 1;
   const maxData = currentPage * perpage;
-
-  // 先建立新陣列
   const data = [];
-  // 這邊將會使用 ES6 forEach 做資料處理
-  // 首先必須使用索引來判斷資料位子，所以要使用 index
+
   pageData.forEach((item, index) => {
-    // 獲取陣列索引，但因為索引是從 0 開始所以要 +1。
     const num = index + 1;
-    // 這邊判斷式會稍微複雜一點
-    // 當 num 比 minData 大且又小於 maxData 就push進去新陣列。
     if (num >= minData && num <= maxData) {
       data.push(item);
     }
   });
-  // 用物件方式來傳遞資料
   const page = {
     pageTotal,
     currentPage,
@@ -166,8 +150,6 @@ function pagination(pageData, nowPage) {
 }
 
 function displayData(data) {
-  cardInfo.innerHTML = ""; //每次區域變化將資料清空
-  areaInfo.innerHTML = "";
   let str = "";
   data.forEach((item) => {
     str += `<div class="col-md-6 col-lg-3 ftco-animate fadeInUp ftco-animated" >
@@ -229,5 +211,6 @@ function switchPage(e) {
   if (e.target.nodeName !== "A") return;
   const page = e.target.dataset.page;
   pagination(pageData, page);
+  window.scrollTo(0,500) ;
 }
 
